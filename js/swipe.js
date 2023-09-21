5c984d7b086907c90a3c4f53d0ad0b1eeb2aa47b
valeri4 <valeriybojko42@gmail.com>
2023-09-21 19:16:09 +0300

    second commit

--- js/swipe.js
+++ js/swipe.js
@@ -2,7 +2,19 @@ new Swiper('.swiper',{
     loop:true,
     pagination:{
         el:'.swiper-pagination',
-        
+        clickable:true,
+    },
+    breakpoints:{
+        320:{
+            slidesPerView:1,
+        },
+        350:{
+            slidesPerView:1.5,
+        },
+        450:{
+            slidesPerView:2,
+        },
+       
     }
     
 });
